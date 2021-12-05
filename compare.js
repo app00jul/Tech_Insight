715275ef23094fc3b1b1a4344838a6fca1301454
Julian Noeske <app00jul@localhost.localdomain>
2021-12-05 18:03:46 -0500

    compare page

--- compare.js
+++ compare.js
@@ -1,26 +1,661 @@
-function updateLinkAndCounter() {
-    var ids = $('.box .prod-id').map(function(i, x) { 
-        return ['P', ++i, '=', $(this).text()].join(''); 
-    }).toArray();
-    
-    $('#container > a').attr('href', 'Compare.html?' + ids.join('&'));
+$('#Laptop_checkbox').prop('checked', true);
+/*Close phone*/
+$("#phone_selected_1").hide();
+$("#phone_selected_2").hide();
+$("#phone_selected_3").hide();
+$("#phone_selected_4").hide();
+$("#phone_selected_5").hide();
+$("#phone_selected_6").hide();
+$("#phone_selected_7").hide();
+$("#phone_selected_8").hide();
+$("#phone_selected_9").hide();
+$("#phone_selected_10").hide();
+$("#phone_selected_11").hide();
+$("#phone_selected_12").hide();
+$("#phone_selected_13").hide();
+$("#phone_selected_14").hide();
+$("#phone_selected_15").hide();
+$("#phone_selected_16").hide();
+$("#phone_selected_17").hide();
+$("#phone_selected_18").hide();
+$("#phone_selected_19").hide();
+$("#phone_selected_20").hide();
+$("#phone_selected_21").hide();
+$("#phone_selected_22").hide();
+$("#phone_selected_23").hide();
+$("#phone_selected_24").hide();
+$("#phone_selected_25").hide();
+$("#phone_selected_26").hide();
+$("#phone_selected_27").hide();
+$("#phone_selected_28").hide();
+$("#phone_selected_29").hide();
+$("#phone_selected_30").hide();
+$("#phone_selected_31").hide();
+$("#phone_selected_32").hide();
+/*Close tablet*/
+$("#tablet_selected_1").hide();
+$("#tablet_selected_2").hide();
+$("#tablet_selected_3").hide();
+$("#tablet_selected_4").hide();
+$("#tablet_selected_5").hide();
+$("#tablet_selected_6").hide();
+$("#tablet_selected_7").hide();
+$("#tablet_selected_8").hide();
+$("#tablet_selected_9").hide();
+$("#tablet_selected_10").hide();
+$("#tablet_selected_11").hide();
+$("#tablet_selected_12").hide();
+$("#tablet_selected_13").hide();
+$("#tablet_selected_14").hide();
+$("#tablet_selected_15").hide();
+$("#tablet_selected_16").hide();
+$("#tablet_selected_17").hide();
+$("#tablet_selected_18").hide();
+$("#tablet_selected_19").hide();
+$("#tablet_selected_20").hide();
+$("#tablet_selected_21").hide();
+$("#tablet_selected_22").hide();
+$("#tablet_selected_22").hide();
+$("#tablet_selected_23").hide();
+$("#tablet_selected_24").hide();
+$("#tablet_selected_25").hide();
+$("#tablet_selected_26").hide();
+$("#tablet_selected_27").hide();
+$("#tablet_selected_28").hide();
+$("#tablet_selected_29").hide();
+$("#tablet_selected_30").hide();
+$("#tablet_selected_31").hide();
+$("#tablet_selected_32").hide();
 
-    var count = $(".box").length;
-    $("p").text(count == 1 ? 'There is 1 box.' : 'There are ' + count + ' boxes.');
-}
-
-$(".more").click(function() {
-    var id=$(this).next('.ProdId').html();
-    $('<div/>', { 'class': 'box' })
-       .append($('<span/>', { class: 'prod-id', text: id }))
-       .append($('<a/>', { href: '#', text: 'x' }))
-       .appendTo('#pop_up_body');
-    
-    updateLinkAndCounter();
-    $("#pop_up_body").removeClass("hidden");
-});
-
-$(".box a").live("click", function() {
-    $(this).parent().remove();
-    updateLinkAndCounter();
-});
+/*Check box*/
+$(function(){
+    /*Laptop*/
+    $("#Laptop_checkbox").click(function(event){
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_1").show();
+        }
+        else{
+            $("#laptop_selected_1").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_2").show();
+        }
+        else{
+            $("#laptop_selected_2").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_3").show();
+        }
+        else{
+            $("#laptop_selected_3").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_4").show();
+        }
+        else{
+            $("#laptop_selected_4").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_5").show();
+        }
+        else{
+            $("#laptop_selected_5").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_6").show();
+        }
+        else{
+            $("#laptop_selected_6").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_7").show();
+        }
+        else{
+            $("#laptop_selected_7").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_8").show();
+        }
+        else{
+            $("#laptop_selected_8").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_9").show();
+        }
+        else{
+            $("#laptop_selected_9").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_10").show();
+        }
+        else{
+            $("#laptop_selected_10").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_11").show();
+        }
+        else{
+            $("#laptop_selected_11").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_12").show();
+        }
+        else{
+            $("#laptop_selected_12").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_13").show();
+        }
+        else{
+            $("#laptop_selected_13").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_14").show();
+        }
+        else{
+            $("#laptop_selected_14").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_15").show();
+        }
+        else{
+            $("#laptop_selected_15").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_16").show();
+        }
+        else{
+            $("#laptop_selected_16").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_17").show();
+        }
+        else{
+            $("#laptop_selected_17").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_18").show();
+        }
+        else{
+            $("#laptop_selected_18").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_19").show();
+        }
+        else{
+            $("#laptop_selected_19").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_20").show();
+        }
+        else{
+            $("#laptop_selected_20").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_21").show();
+        }
+        else{
+            $("#laptop_selected_21").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_22").show();
+        }
+        else{
+            $("#laptop_selected_22").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_23").show();
+        }
+        else{
+            $("#laptop_selected_23").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_24").show();
+        }
+        else{
+            $("#laptop_selected_24").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_25").show();
+        }
+        else{
+            $("#laptop_selected_25").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_26").show();
+        }
+        else{
+            $("#laptop_selected_26").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_27").show();
+        }
+        else{
+            $("#laptop_selected_27").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_28").show();
+        }
+        else{
+            $("#laptop_selected_28").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_29").show();
+        }
+        else{
+            $("#laptop_selected_29").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_30").show();
+        }
+        else{
+            $("#laptop_selected_30").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_31").show();
+        }
+        else{
+            $("#laptop_selected_31").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#laptop_selected_32").show();
+        }
+        else{
+            $("#laptop_selected_32").hide();
+        }
+    })
+/*----------------------------------------------------------------------------------------------------------*/
+/*__________________________________________________________________________________________________________*/
+    /*Phone*/    
+    $("#Phone_checkbox").click(function(event){
+        if ($(this).is(":checked")) {
+            $("#phone_selected_1").show();
+        }
+        else{
+            $("#phone_selected_1").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_2").show();
+        }
+        else{
+            $("#phone_selected_2").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_3").show();
+        }
+        else{
+            $("#phone_selected_3").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_4").show();
+        }
+        else{
+            $("#phone_selected_4").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_5").show();
+        }
+        else{
+            $("#phone_selected_5").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_6").show();
+        }
+        else{
+            $("#phone_selected_6").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_7").show();
+        }
+        else{
+            $("#phone_selected_7").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_8").show();
+        }
+        else{
+            $("#phone_selected_8").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_9").show();
+        }
+        else{
+            $("#phone_selected_9").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_10").show();
+        }
+        else{
+            $("#phone_selected_10").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_11").show();
+        }
+        else{
+            $("#phone_selected_11").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_12").show();
+        }
+        else{
+            $("#phone_selected_12").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_13").show();
+        }
+        else{
+            $("#phone_selected_13").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_14").show();
+        }
+        else{
+            $("#phone_selected_14").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_15").show();
+        }
+        else{
+            $("#phone_selected_15").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_16").show();
+        }
+        else{
+            $("#phone_selected_16").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_17").show();
+        }
+        else{
+            $("#phone_selected_17").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_18").show();
+        }
+        else{
+            $("#phone_selected_18").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_19").show();
+        }
+        else{
+            $("#phone_selected_19").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_20").show();
+        }
+        else{
+            $("#phone_selected_20").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_21").show();
+        }
+        else{
+            $("#phone_selected_21").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_22").show();
+        }
+        else{
+            $("#phone_selected_22").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_23").show();
+        }
+        else{
+            $("#phone_selected_23").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_24").show();
+        }
+        else{
+            $("#phone_selected_24").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_25").show();
+        }
+        else{
+            $("#phone_selected_25").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_26").show();
+        }
+        else{
+            $("#phone_selected_26").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_27").show();
+        }
+        else{
+            $("#phone_selected_27").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_28").show();
+        }
+        else{
+            $("#phone_selected_28").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_29").show();
+        }
+        else{
+            $("#phone_selected_29").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_30").show();
+        }
+        else{
+            $("#phone_selected_30").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_31").show();
+        }
+        else{
+            $("#phone_selected_31").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#phone_selected_32").show();
+        }
+        else{
+            $("#phone_selected_32").hide();
+        }
+    })
+/*----------------------------------------------------------------------------------------------------------*/
+/*__________________________________________________________________________________________________________*/
+    /*Tablet*/
+    $("#Tablet_checkbox").click(function(event){
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_1").show();
+        }
+        else{
+            $("#tablet_selected_1").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_2").show();
+        }
+        else{
+            $("#tablet_selected_2").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_3").show();
+        }
+        else{
+            $("#tablet_selected_3").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_4").show();
+        }
+        else{
+            $("#tablet_selected_4").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_5").show();
+        }
+        else{
+            $("#tablet_selected_5").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_6").show();
+        }
+        else{
+            $("#tablet_selected_6").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_7").show();
+        }
+        else{
+            $("#tablet_selected_7").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_8").show();
+        }
+        else{
+            $("#tablet_selected_8").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_9").show();
+        }
+        else{
+            $("#tablet_selected_9").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_10").show();
+        }
+        else{
+            $("#tablet_selected_10").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_11").show();
+        }
+        else{
+            $("#tablet_selected_11").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_12").show();
+        }
+        else{
+            $("#tablet_selected_12").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_13").show();
+        }
+        else{
+            $("#tablet_selected_13").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_14").show();
+        }
+        else{
+            $("#tablet_selected_14").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_15").show();
+        }
+        else{
+            $("#tablet_selected_15").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_16").show();
+        }
+        else{
+            $("#tablet_selected_16").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_17").show();
+        }
+        else{
+            $("#tablet_selected_17").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_18").show();
+        }
+        else{
+            $("#tablet_selected_18").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_19").show();
+        }
+        else{
+            $("#tablet_selected_19").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_20").show();
+        }
+        else{
+            $("#tablet_selected_20").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_21").show();
+        }
+        else{
+            $("#tablet_selected_21").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_22").show();
+        }
+        else{
+            $("#tablet_selected_22").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_23").show();
+        }
+        else{
+            $("#tablet_selected_23").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_24").show();
+        }
+        else{
+            $("#tablet_selected_24").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_25").show();
+        }
+        else{
+            $("#tablet_selected_25").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_26").show();
+        }
+        else{
+            $("#tablet_selected_26").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_27").show();
+        }
+        else{
+            $("#tablet_selected_27").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_28").show();
+        }
+        else{
+            $("#tablet_selected_28").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_29").show();
+        }
+        else{
+            $("#tablet_selected_29").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_30").show();
+        }
+        else{
+            $("#tablet_selected_30").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_31").show();
+        }
+        else{
+            $("#tablet_selected_31").hide();
+        }
+        if ($(this).is(":checked")) {
+            $("#tablet_selected_32").show();
+        }
+        else{
+            $("#tablet_selected_32").hide();
+        }
+    })
+})
